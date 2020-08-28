72d0f69b7679646efedb2630a5bacdd3707a8c32
nishio <nishio.ryota.1997@gmail.com>
2020-08-28 22:22:18 +0900

    test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,6 @@
 class LoginController < ApplicationController
     def login
-      login_user = User.find_by(email:params[:email],password:params[:password])
+      login_user = User.find_by(email: params[:email], password: params[:password])
       if login_user != nil
         render plain: login_user.token
       else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'login/login'
+  post 'login/login'
   resources :users
   resources :memos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
